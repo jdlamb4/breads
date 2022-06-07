1351d8b76bcfa06904ee00d3aae85a79444e1d65
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-06 20:53:57 -0400

    finished BE 5.3 code along edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -27,6 +27,13 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
 
 // SHOW   using the value put into the query parameter to decide what to display on webpage
 breads.get('/:arrayIndex', (req, res) => {
@@ -60,6 +67,17 @@ breads.post('/', (req, res) => {
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 // DELETE
--- views/show.jsx
+++ views/show.jsx
@@ -17,6 +17,8 @@ function Show({ bread, index }) {
       <li>
         <a href="/breads">Go home</a>
       </li>
+      
+      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
