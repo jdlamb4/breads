b6427712a95fd66a26ce17711436af8742c0e227
Jacob Lamb <jacoblamb15@gmail.com>
2022-06-11 22:22:23 -0400

    BE-8 -- Helper Methods Part 2

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -28,11 +28,13 @@ breads.get('/new', (req, res) => {
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
 })
 
 
@@ -72,20 +74,25 @@ breads.post('/', (req, res) => {
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
 })
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -17,10 +17,8 @@ function Show({ bread, index }) {
       <li>
         <a href="/breads">Go home</a>
       </li>
-      
-      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-
-      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>
     </Default>
